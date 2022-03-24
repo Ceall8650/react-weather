89325ab5711a09be92cfd2dd55b9950442773802
Ceall8650 <ceall8650@gmail.com>
2022-03-25 00:11:22 +0800

    Implement weather card layout and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
+import { ReactComponent as DayCloudIcon } from "./images/day-cloudy.svg";
+import { ReactComponent as RainIcon } from "./images/rain.svg";
+import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="flex justify-center items-center h-full bg-container">
+			<div className="relative min-w-card bg-card shadow-card box-border py-8 px-4">
+				<h1 className="text-[28px] text-title mb-5">Taipei</h1>
+				<div className="text-default text-lg mb-8">多雲時晴</div>
+				<div className="flex justify-between items-center mb-8">
+					<div className="flex text-8xl text-temperature font-light">
+						23 <span className="font-normal text-5xl">°C</span>
+					</div>
+					<DayCloudIcon />
+				</div>
+				<div className="flex items-center text-base font-light text-default mb-5">
+					<AirFlowIcon className="w-6 h-auto mr-7" /> 23 m/h
+				</div>
+				<div className="flex items-center text-base font-light text-default">
+					<RainIcon className="w-6 h-auto mr-7" /> 48%
+				</div>
+				<div className="absolute right-4 bottom-4 text-xs inline-flex items-end text-default">
+					最後觀測時間：上午 12:03{" "}
+					<RefreshIcon className="ml-3 w-4 h-4 cursor-pointer" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        card: '0 1px 3px 0 #999999'
+      },
+      colors: {
+        card: '#f9f9f9',
+        container: '#ededed',
+        default: '#828282',
+        temperature: '#757575',
+        title: '#212121',
+      },
+      minWidth: {
+        card: '360px'
+      }
+    },
   },
   plugins: [],
   ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
