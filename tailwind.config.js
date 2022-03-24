0fdb291de8b8d0f4aed3196e9965cd6213dfcdf4
Ceall8650 <ceall8650@gmail.com>
2022-03-25 00:10:55 +0800

    Initial tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+}
